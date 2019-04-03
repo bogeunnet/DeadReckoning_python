e211b88513ddaed9af0e9f8bf26722490938487a
Steve B. Kim (Bogeun Kim) <bogeun.kim@alumni.usc.edu>
2019-04-03 14:47:36 +0900

    Update Find_accumulate_xy_coordinate.py

--- Find_accumulate_xy_coordinate.py
+++ Find_accumulate_xy_coordinate.py
@@ -4,16 +4,8 @@ from scipy.special._ufuncs import radian
 
 # 싼타페 DM의 윤거전 = 1628 mm = 1.628 meter
 # 윤거전 : 전륜 바퀴 2개에 대해, 각각 바퀴의 중심 위치간의 거리
-# 100km 고속주행시 양 바퀴의 속도가 약 0.1km 차이일때
-# 왼쪽 바퀴는 100.1 km / h, 오른쪽바퀴는 100.2km 라고하면
-# 100.1 * 1000 / 3600 ==> meter/sec
-# 100.2 * 1000 / 3600 ==> meter/sec
-# FL-FR = 5.56 - 5.578
 
-print(math.degrees(math.atan2(1.628, 0)))
-print(90 - math.degrees(math.atan2(1.628, -0.018)))
-
-Tire_width = 1.628     # 김보근싼타페
+Tire_width = 1.628     # 싼타페 윤거전
 
 whole_x = 0         # initial_x_coordinate
 whole_y = 0         # initial_y_coordinate
@@ -23,7 +15,8 @@ lower_var = 18      # km/h to meter/0.2sec
 # lower_var = 18/5  # km/h to meter/1sec
 print(whole_x, whole_y, whole_angle, delta_angle)
 
-# --------------------------------------------
+# -------------------------------------------- 1회 데이터 인입시
+# -------------------------------------------- Data update First
 FL = 30.2 / lower_var  # 30.2 km 라면, (30.2 * 1000 / (3600) ) / 5 ==> 30.2 / 18
 FR = 30.3 / lower_var  # 30.2 km 라면, (30.3 * 1000 / (3600) ) / 5 ==> 30.3 / 18
 delta_angle = 90 - math.degrees(math.atan2(Tire_width, FR - FL))
@@ -33,7 +26,8 @@ whole_x = whole_x + distance * math.cos(math.radians(whole_angle))
 whole_y = whole_y + distance * math.sin(math.radians(whole_angle))
 print(whole_x, whole_y, whole_angle, delta_angle)
 
-# --------------------------------------------
+# -------------------------------------------- 2회 데이터 인입시
+# -------------------------------------------- Data update Second
 FL = 30.7 / lower_var  # 30.2 km 라면, (30.2 * 1000 / (3600) ) / 5 ==> 30.2 / 18
 FR = 30.5 / lower_var  # 30.2 km 라면, (30.3 * 1000 / (3600) ) / 5 ==> 30.3 / 18
 delta_angle = 90 - math.degrees(math.atan2(Tire_width, FR - FL))
@@ -43,7 +37,8 @@ whole_x = whole_x + distance * math.cos(math.radians(whole_angle))
 whole_y = whole_y + distance * math.sin(math.radians(whole_angle))
 print(whole_x, whole_y, whole_angle, delta_angle)
 
-# --------------------------------------------
+# -------------------------------------------- 3회 데이터 인입시
+# -------------------------------------------- Data update Third
 FL = 30.1 / lower_var  # 30.2 km 라면, (30.2 * 1000 / (3600) ) / 5 ==> 30.2 / 18
 FR = 30.5 / lower_var  # 30.2 km 라면, (30.3 * 1000 / (3600) ) / 5 ==> 30.3 / 18
 delta_angle = 90 - math.degrees(math.atan2(Tire_width, FR - FL))
